d07d872a27759afe6a54426b407da8b4b949f313
Nicholas  Blackwell <nblackwe@Nicholass-Mac-mini.local>
2018-04-13 08:31:35 -0700

    add log

--- index.js
+++ index.js
@@ -20,68 +20,76 @@ if (config.websocketPort !== false) {
 
 
 
-
-
-
-	var temperatureSensors={}
-	var counter=0;
+	var fs = require('fs');
+	var temperatureSensors = {}
+	var counter = 0;
 
 	var Temperature = require('node-rpio-temperature');
 	var wsserver;
-	
-	var devices =require('./devices.json');
+
+	var devices = require('./devices.json');
 
 	//(new Temperature.Mock(devices)).on('update', function(sensor) {
 	(new Temperature()).on('update', function(sensor) {
-	// 
+		// 
 		//sensor.time=(new Date()).valueOf();
-		temperatureSensors[sensor.device]=sensor;
+		temperatureSensors[sensor.device] = sensor;
 		//console.log(JSON.stringify(sensor));
 		counter++;
 
 		wsserver.broadcast('notification.statechange', JSON.stringify(sensor));
 
 
+
+		fs.appendFile('.history.'+sensor.device + '.json', JSON.stringify({
+			"time": (new Date()).valueOf(),
+			"value": sensor.value
+		}) + ",\n", function(err) {
+			if (err) {
+				throw err;
+			}
+			//console.log('Saved!');
+		});
+
+
+
 	});
 
-	
-	
 
 
-
-	var wsserver=(new(require('tinywebsocketjs'))({
+	var wsserver = (new(require('tinywebsocketjs'))({
 		port: config.websocketPort
 	})).addTask('list_devices', function(options, callback) {
 
-		var additionalDevices=JSON.parse(JSON.stringify(devices));
-		
+		var additionalDevices = JSON.parse(JSON.stringify(devices));
+
 		console.log(JSON.stringify(additionalDevices));
-		
 
-		callback(Object.keys(temperatureSensors).map(function(sensorId){
 
-			var device=temperatureSensors[sensorId];
+		callback(Object.keys(temperatureSensors).map(function(sensorId) {
 
-			
+			var device = temperatureSensors[sensorId];
 
-			additionalDevices.forEach(function(d, i){
-				if(d.device==sensorId){
-					additionalDevices.splice(i,1);
-					Object.keys(d).forEach(function(k){
-						device[k]=d[k];
+
+
+			additionalDevices.forEach(function(d, i) {
+				if (d.device == sensorId) {
+					additionalDevices.splice(i, 1);
+					Object.keys(d).forEach(function(k) {
+						device[k] = d[k];
 					});
 				}
 			});
 
 			return {
 
-				"name":device.name || "Temp: "+device.device,
-				"device":sensorId,
-				"type":"float",
-				"state":device.value,
-				"readonly":true,
-				"graphOptions":device.graphOptions||{},
-				"units":device.units
+				"name": device.name || "Temp: " + device.device,
+				"device": sensorId,
+				"type": "float",
+				"state": device.value,
+				"readonly": true,
+				"graphOptions": device.graphOptions || {},
+				"units": device.units
 			}
 
 		}).concat(additionalDevices));
